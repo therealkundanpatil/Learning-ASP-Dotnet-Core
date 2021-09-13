ae10dfd138e8693d3a6c9c931d67bbdaca19bad8
therealkundanpatil <kp28021996@gmail.com>
2021-09-13 09:03:12 -0400

    Commit Message
    + Added Book class and defined few properties

---
+++ BookListRazor/BookListRazor/Model/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class Book
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        public string name { get; set; }
+        
+        public string author { get; set; }
+    }
+}
