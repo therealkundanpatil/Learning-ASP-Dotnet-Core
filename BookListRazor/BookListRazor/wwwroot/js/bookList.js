3e24c5dec6e8662a44cc7f1ddfe37f31ac977b47
therealkundanpatil <kp28021996@gmail.com>
2021-09-16 18:02:38 -0400

    Commit Message
    + Some asthetic changes

--- BookListRazor/BookListRazor/wwwroot/js/bookList.js
+++ BookListRazor/BookListRazor/wwwroot/js/bookList.js
@@ -19,9 +19,9 @@ function loadDataTable() {
                 "data": "id",
                 "render": function (data) {
                     return `<div class = "text-center">
-                                <a href="/BookList/Edit?id=${data}" class="btn btn-success text-white" style="cursor:pointer;width:70px;">Edit</a>
-                                &nbsp;
-                                <a class="btn btn-danger text-white" style="cursor:pointer;width:70px;">Delete</a>
+                            <a class="btn btn-danger text-white" style="cursor:pointer;width:70px;" onclick="Delete("/api/Book?id=${data}")">Delete</a>
+                            &nbsp;
+                            <a href="/BookList/Edit?id=${data}" class="btn btn-success text-white" style="cursor:pointer;width:70px;">Edit</a>    
                             </div>`;
                 },
                 "width": "40%"
@@ -31,5 +31,31 @@ function loadDataTable() {
             "emptyTable": "no data found"
         },
         "width": "100%",
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you sure ?",
+        text: "Once deleted, you will not be able to recover",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
     });
 }
